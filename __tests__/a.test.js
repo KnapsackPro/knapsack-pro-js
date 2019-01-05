af8e9d2c8795e5503f963681368c2263fbb08c46
Artur Trzop <arturtrzop@gmail.com>
2019-01-05 21:38:29 +0100

    Add delay to test

--- __tests__/a.test.js
+++ __tests__/a.test.js
@@ -1,3 +1,10 @@
+const sleep = waitTimeInMs =>
+  new Promise(resolve => setTimeout(resolve, waitTimeInMs));
+
 it("a test", () => {
   expect(2 + 2).toBe(4);
+});
+
+it("sleep", async () => {
+  await sleep(2000);
 });
