01a2cfca79d2b8e6ef6d2ee5da942de7c4b1bf04
3v0k4 <riccardo.odone@gmail.com>
2023-12-18 16:38:35 +0100

    ci: use circleci 2.1

--- packages/cypress-example-test-suite/cypress/e2e/knapsack-pro.cy.js
+++ packages/cypress-example-test-suite/cypress/e2e/knapsack-pro.cy.js
@@ -1,5 +1,15 @@
 describe('knapsackpro.com', () => {
   it('visits Knapsack Pro', () => {
+    cy.on('uncaught:exception', (error) => {
+      if (error.message.includes('Failed to resolve module specifier "application"')) {
+        // knapsackpro.com polyfills ESModules,
+        // but it still throws an error in the console connected with:
+        // <script type="module">import "application"</script>
+        return false // ignore error
+      }
+
+      return true
+    })
     cy.visit('http://knapsackpro.com/')
     cy.contains('Knapsack Pro')
   })
