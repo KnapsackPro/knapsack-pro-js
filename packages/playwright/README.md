3d291abc537ffb1a0e011fad501bf2bbc1b7aa96
3v0k4 <riccardo.odone@gmail.com>
2025-10-03 16:23:44 +0200

    chore: playwright skeleton

---
+++ packages/playwright/README.md
@@ -0,0 +1,64 @@
+# @knapsack-pro/playwright
+
+<p align="center">
+  <a href="https://knapsackpro.com?utm_source=github&utm_medium=readme&utm_campaign=knapsack-pro-playwright&utm_content=hero_logo">
+    <img alt="Knapsack Pro" src="./.github/assets/logo.png" width="100%" style="max-width: 100%;" />
+  </a>
+</p>
+
+<h3 align="center">Speed up your tests</h3>
+<p align="center">Run your 1-hour test suite in 2 minutes with optimal parallelisation on your existing CI infrastructure</p>
+
+---
+
+<div align="center">
+  <a href="https://circleci.com/gh/KnapsackPro/knapsack-pro-js">
+    <img alt="Circle CI" src="https://circleci.com/gh/KnapsackPro/knapsack-pro-js.svg?style=svg" />
+  </a>
+</div>
+
+<br />
+<br />
+
+Knapsack Pro wraps [Playwright](https://playwright.dev/) and works with your existing CI infrastructure to parallelize tests optimally:
+
+- Dynamically splits your tests based on up-to-date test execution data
+- Is designed from the ground up for CI and supports all of them
+- Tracks your CI builds to detect bottlenecks
+- Does not have access to your source code and collects minimal test data
+- Enables you to export historical metrics about your CI builds
+- Replaces local dependencies like Redis with an API and runs your tests regardless of network problems
+
+## Installation
+
+See the [docs](https://docs.knapsackpro.com/playwright/guide/) to get started:
+
+<div align="center">
+  <a href="https://docs.knapsackpro.com/playwright/guide/">
+    <img alt="Install button" src="./.github/assets/install-button.png" width="116" height="50" />
+  </a>
+</div>
+
+## Dependencies
+
+- [@knapsack-pro/core](https://github.com/KnapsackPro/knapsack-pro-js/tree/main/packages/core)
+
+## Contributing
+
+Follow the steps in the [root README.md](https://github.com/KnapsackPro/knapsack-pro-js#contributing) to set up the project.
+
+You can compile TypeScript in watch mode from the root folder with:
+
+```bash
+npm start -w packages/playwright
+```
+
+### Testing
+
+To test `@knapsack-pro/playwright` against a real test suite we use:
+
+- [playwright-example-test-suite](https://github.com/KnapsackPro/knapsack-pro-js/tree/main/packages/playwright-example-test-suite)
+
+### Publishing
+
+See [Publishing](../../#publishing) in the root README.md.
---
+++ packages/playwright/src/knapsack-playwright.ts
@@ -0,0 +1,173 @@
+#!/usr/bin/env node
+
+// import {
+//   KnapsackProCore,
+//   KnapsackProLogger,
+//   onQueueFailureType,
+//   onQueueSuccessType,
+//   TestFile,
+// } from '@knapsack-pro/core';
+//
+// import {
+//   startVitest,
+//   parseCLI,
+//   ResolvedConfig,
+//   Vitest,
+//   resolveConfig,
+// } from 'vitest/node';
+// import { v4 as uuidv4 } from 'uuid';
+// import { minimatch } from 'minimatch';
+// import { glob } from 'glob';
+// import { join, dirname, relative } from 'path';
+// import { readFileSync } from 'fs';
+// import { fileURLToPath } from 'url';
+// import * as Urls from './urls.js';
+//
+// if (process.env.KNAPSACK_PRO_TEST_SUITE_TOKEN_VITEST) {
+//   process.env.KNAPSACK_PRO_TEST_SUITE_TOKEN =
+//     process.env.KNAPSACK_PRO_TEST_SUITE_TOKEN_VITEST;
+// }
+//
+// const knapsackProLogger = new KnapsackProLogger();
+//
+// async function main() {
+//   const filePath = fileURLToPath(import.meta.url);
+//   const dirName = dirname(filePath);
+//   const pkg = JSON.parse(
+//     readFileSync(join(dirName, '..', 'package.json'), 'utf8'),
+//   );
+//   knapsackProLogger.debug(`Running ${pkg.name}@${pkg.version}`);
+//
+//   const cliArguments = parseCLI(`vitest ${process.argv.slice(2).join(' ')}`);
+//   if (cliArguments.filter.length > 0) {
+//     throw new Error(
+//       '[@knapsack-pro/vitest] Passing list of files to test from command line is not supported',
+//     );
+//   }
+//
+//   const resolvedConfig = await resolveConfig();
+//
+//   const knapsackPro = new KnapsackProCore(
+//     pkg.name,
+//     pkg.version,
+//     makeGetAllTestFiles(resolvedConfig.vitestConfig),
+//   );
+//
+//   const onSuccess: onQueueSuccessType = async (testFiles: TestFile[]) => {
+//     const filters = testFiles.map((testFile) => testFile.path);
+//     const cliOptions = {
+//       ...cliArguments.options,
+//       outputFile: withBatchedBlobOutputFile(
+//         cliArguments,
+//         resolvedConfig.vitestConfig,
+//       ),
+//       watch: false,
+//     };
+//     knapsackProLogger.debug(`Filters: ${JSON.stringify(filters)}`);
+//     knapsackProLogger.debug(`CLIOptions: ${JSON.stringify(cliOptions)}`);
+//     const vitest = await startVitest('test', filters, cliOptions);
+//
+//     if (!vitest) {
+//       throw new Error('[@knapsack-pro/vitest] Vitest failed to start');
+//     }
+//
+//     return getTestResults(vitest);
+//   };
+//
+//   const onError: onQueueFailureType = () => {};
+//
+//   knapsackPro.runQueueMode(onSuccess, onError);
+// }
+//
+// main();
+//
+// /**
+//  * Processes the test results and converts them to a format vitest
+//  * understands
+//  * @param vitest
+//  */
+// function getTestResults(vitest: Vitest) {
+//   const recordedTestFiles = vitest.state.getFiles().map<TestFile>((file) => {
+//     const testFile = vitest.state.getReportedEntity(file);
+//
+//     if (testFile?.type !== 'module') {
+//       throw new Error('[@knapsack-pro/vitest] Vitest reported non-module file');
+//     }
+//
+//     return {
+//       path: relative(vitest.config.root, file.filepath),
+//       time_execution: testFile.diagnostic().duration / 1000,
+//     };
+//   });
+//
+//   const isTestSuiteGreen = process.exitCode !== 1;
+//
+//   return {
+//     recordedTestFiles,
+//     isTestSuiteGreen,
+//   };
+// }
+//
+// /**
+//  * Returns a function that returns all test files in the test suite
+//  */
+// function makeGetAllTestFiles(resolvedConfig: ResolvedConfig) {
+//   return () => {
+//     const testFileIncludePattern =
+//       process.env.KNAPSACK_PRO_TEST_FILE_PATTERN ||
+//       resolvedConfig.include ||
+//       '**/*.{test,spec}.?(c|m)[jt]s?(x)';
+//
+//     const testFileExcludePattern = process.env
+//       .KNAPSACK_PRO_TEST_FILE_EXCLUDE_PATTERN
+//       ? [process.env.KNAPSACK_PRO_TEST_FILE_EXCLUDE_PATTERN]
+//       : resolvedConfig.exclude || [];
+//
+//     const testFiles = glob
+//       .sync(testFileIncludePattern)
+//       .filter((testFilePath) =>
+//         testFileExcludePattern.every(
+//           (excludePattern) =>
+//             !minimatch(testFilePath, excludePattern, {
+//               matchBase: true,
+//             }),
+//         ),
+//       )
+//       .filter((testFilePath) => !testFilePath.match(/node_modules/))
+//       .map((testFilePath) => ({ path: testFilePath }));
+//
+//     if (testFiles.length === 0) {
+//       const errorMessage = `[@knapsack-pro/vitest] Test files cannot be found.\nPlease set KNAPSACK_PRO_TEST_FILE_PATTERN matching your test directory structure.\nLearn more: ${Urls.NO_TESTS_FOUND}`;
+//
+//       knapsackProLogger.error(errorMessage);
+//       throw new Error(errorMessage);
+//     }
+//
+//     return testFiles;
+//   };
+// }
+//
+// // Vitest uses the following priority when parsing the outputFile:
+// // - From CLI if string (e.g., vitest --outputFile=file.txt)
+// // - From config if string (e.g., outputFile: 'file.txt')
+// // - Merge objects; Example:
+// //     Input: vitest --outputFile.junit=junit.txt && outputFile: { json: 'json.txt', junit: 'ignored.txt' }
+// //     Output: { json: 'json.txt', junit: 'junit.txt' }
+// function withBatchedBlobOutputFile(
+//   cliArguments: ReturnType<typeof parseCLI>,
+//   resolvedConfig: ResolvedConfig,
+// ): ReturnType<typeof parseCLI>['options']['outputFile'] {
+//   const cliOutputFile = cliArguments.options.outputFile;
+//   if (typeof cliOutputFile === 'string') return cliOutputFile;
+//   const configOutputFile = resolvedConfig.outputFile;
+//   if (typeof configOutputFile === 'string') return configOutputFile;
+//   const filename = [
+//     process.env.KNAPSACK_PRO_CI_NODE_INDEX,
+//     process.env.KNAPSACK_PRO_CI_NODE_TOTAL,
+//     uuidv4(),
+//   ].join('-');
+//   const blob = `.vitest-reports/blob-${filename}.json`;
+//   return { blob, ...configOutputFile, ...cliOutputFile };
+// }
+
+console.log('pw');
