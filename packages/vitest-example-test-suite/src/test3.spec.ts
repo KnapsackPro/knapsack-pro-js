b4947c8d2c3c8d62555820bd6dbd16ab8f845648
3v0k4 <riccardo.odone@gmail.com>
2025-10-03 15:53:36 +0200

    chore: move prettier to root

--- packages/vitest-example-test-suite/src/test3.spec.ts
+++ packages/vitest-example-test-suite/src/test3.spec.ts
@@ -1,7 +1,7 @@
-import { expect, test } from "vitest";
-import { add } from "./add";
+import { expect, test } from 'vitest';
+import { add } from './add';
 
 test('adds 1 + 2 to equal 3', async () => {
-    await new Promise((resolve) => setTimeout(resolve, 10));
-    expect(add(1, 2)).toBe(3);
+  await new Promise((resolve) => setTimeout(resolve, 10));
+  expect(add(1, 2)).toBe(3);
 });
